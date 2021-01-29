ad6e877ad3c3019f50a8ac5b2078e9277f71db66
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-01-29 23:15:26 +0300

    done first lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { GirlsListComponent } from './girls-list/girls-list.component';
+import { HomeComponent } from './home/home.component';
+
+import {SomeDataService} from './services/some.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'girls', component: GirlsListComponent},
+  { path: 'pizza', component: PizzaListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaListComponent,
+    GirlsListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    SomeDataService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/services/some.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+interface KeyValuePair {
+  key:string;
+  value:any;
+}
+
+@Injectable({ providedIn: 'root' })
+export class SomeDataService {
+  public data: number = 123;
+  public globalArray: KeyValuePair[];
+}
+
